7d3ebc4b23e0d3533c641bb068d65bf910cdf5df
Fergus Cooper <fcooper8472@gmail.com>
2020-07-08 18:49:18 +0100

    Alter setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 mkdir -p ~/.streamlit/
-echo "[general]\nemail = \"fcooper8472@gmail.com\"\n" > ~/.streamlit/credentials.toml
-echo "[server]\nheadless = true\nenableCORS=false\nport = $PORT\n" > ~/.streamlit/credentials.toml
+printf "[general]\nemail=\"fcooper8472@gmail.com\"\n" > ~/.streamlit/credentials.toml
+printf "[server]\nheadless=true\nenableCORS=false\nport=%s\n" "${PORT}" > ~/.streamlit/config.toml
