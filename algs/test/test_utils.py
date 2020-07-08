452c09347d3683191082a426053161ea68c83dac
Fergus Cooper <fcooper8472@gmail.com>
2020-07-08 22:30:55 +0100

    Create python package and add testing infra

---
+++ algs/test/test_utils.py
@@ -0,0 +1,5 @@
+from algs import greet
+
+
+def test_answer():
+    assert greet('world') == "Hello world"
---
+++ algs/utils.py
@@ -0,0 +1,8 @@
+def greet(name=""):
+    """
+    A function that takes a name and returns a greeting.
+
+    :param name: the name to greet
+    :return: the greeting
+    """
+    return "Hello " + name
